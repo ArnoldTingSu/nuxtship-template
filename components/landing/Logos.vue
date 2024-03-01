8164d392f8f6eba92f6fb751c7d46981e40a2a2d
Arnold Su <arnoldtingsu@gmail.com>
2024-02-29 16:23:45 -0800

    arrangement on landing page

--- components/landing/Logos.vue
+++ components/landing/Logos.vue
@@ -5,12 +5,14 @@
       class="flex gap-8 md:gap-20 items-center justify-center mt-10 flex-wrap"
     >
       <Icon class="h-8 md:h-14" name="simple-icons:nuxtdotjs" size="48" />
-      <Icon class="h-8 md:h-14" name="simple-icons:tailwindcss" size="48" />
       <Icon class="h-8 md:h-14" name="simple-icons:vuedotjs" size="48" />
       <Icon class="h-8 md:h-14" name="simple-icons:python" size="48" />
-      <Icon class="h-8 md:h-12" name="simple-icons:solidity" size="48" />
+      <Icon class="h-8 md:h-12" name="simple-icons:openai" size="48" />
+      <Icon class="h-8 md:h-14" name="simple-icons:tailwindcss" size="48" />
+      <Icon class="h-8 md:h-14" name="simple-icons:firebase" size="48" />
       <Icon class="h-8 md:h-12" name="simple-icons:django" size="48" />
-      <Icon class="h-8 md:h-12" name="simple-icons:cplusplus" size="48" />
+      <Icon class="h-8 md:h-14" name="simple-icons:sqlite" size="48" />
+      <Icon class="h-8 md:h-12" name="simple-icons:solidity" size="48" />
     </div>
   </div>
 </template>
